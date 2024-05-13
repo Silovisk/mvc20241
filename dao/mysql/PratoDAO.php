a330b257b88ff414bf5bc1ba6a9d9e7f62f3efb8
Silovisk <silasaraujo1@hotmail.com>
2024-05-12 23:26:26 -0300

    refactor: Atualizar PratoDAO para retornar dados do restaurante junto com o prato

--- dao/mysql/PratoDAO.php
+++ dao/mysql/PratoDAO.php
@@ -55,44 +55,57 @@ class PratoDAO extends MysqlFactory implements IPratoDAO
         if (empty($prato)) {
             return "Prato não encontrado";
         }
-        $sql = "SELECT * FROM Pratos where id = $id";
-        $retorno = $this->banco->executar($sql);
-        return $retorno;
+        $sqlPrato = "SELECT * FROM Pratos where id = $id";
+        // $sql = "SELECT p.*, r.* FROM Pratos p INNER JOIN restaurantes r ON r.id = p.restaurante_id where p.id = $id";
+        $retornoPrato = $this->banco->executar($sqlPrato);
+
+        $restaurante_id = $retornoPrato[0]['restaurante_id'];
+        $sqlRestaurante = "SELECT * FROM Restaurantes where id = $restaurante_id";
+        $retornoRestaurante = $this->banco->executar($sqlRestaurante);
+
+        $data = [
+            "prato" => $retornoPrato,
+            "restaurante" => $retornoRestaurante
+        ];
+        return $data;
     }
 
     public function update($id, $nome, $descricao, $preco, $restaurante_id)
     {
-        $prato = $this->findPrato($id);
-        $restauranteDAO = new RestauranteDAO();
-        $restaurante = $restauranteDAO->findRestaurante($restaurante_id);
+        try {
+            $prato = $this->findPrato($id);
+            $restauranteDAO = new RestauranteDAO();
+            $restaurante = $restauranteDAO->findRestaurante($restaurante_id);
 
-        if (empty($prato)) {
-            return "Prato não encontrado";
+            if (empty($prato)) {
+                return "Prato não encontrado";
+            }
+
+            if (empty($restaurante)) {
+                return "Restaurante não encontrado";
+            }
+
+            $sql ="UPDATE Pratos SET 
+                    nome = :nome
+                    ,descricao = :descricao
+                    ,preco = :preco
+                    ,restaurante_id = :restaurante_id 
+                    WHERE id = :id
+                ";
+            $retorno = $this->banco->executar(
+                $sql,
+                [
+                    "nome" => $nome,
+                    "descricao" => $descricao,
+                    "preco" => $preco,
+                    "restaurante_id" => $restaurante_id,
+                    "id" => $id
+                ]
+            );
+            return $retorno;
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
-
-        if (empty($restaurante)) {
-            return "Restaurante não encontrado";
-        }
-
-        $sql =
-            "
-                UPDATE Pratos SET 
-                nome = :nome
-                ,descricao = :descricao
-                ,preco = :preco
-                ,restaurante_id = :restaurante_id 
-                WHERE id = :id
-            ";
-        $retorno = $this->banco->executar(
-            $sql,
-            [
-                "nome" => $nome,
-                "descricao" => $descricao,
-                "preco" => $preco,
-                "restaurante_id" => $restaurante_id
-            ]
-        );
-        return $retorno;
     }
 
     public function destroy($id)
