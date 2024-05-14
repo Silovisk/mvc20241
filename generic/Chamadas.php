d912fba20ee26eaed60af8e6167e129047506dad
Humberto Patrick Lacerda Ribeiro <humbertop@liae.edu.uniube.br>
2024-05-14 20:30:37 -0300

    termino do jwt

--- generic/Chamadas.php
+++ generic/Chamadas.php
@@ -11,7 +11,8 @@ class Chamadas
             "professores/lista" => new Acao("service\ProfessorService", "listar"),
             "professores/soma" => new Acao("service\ProfessorService", "professor",[Acao::GET,Acao::POST]),
             "professores/inserir" => new Acao("service\ProfessorService", "inserir",[Acao::POST]),
-            "usuarios/autenticar" => new Acao("service\UsuariosService", "autenticar",[Acao::POST]),
+            "usuarios/autenticar" => new Acao("service\UsuariosService", "autenticar",[Acao::POST],false),
+            "alunos/autenticar" => new Acao("service\AlunosService", "autenticar",[Acao::POST],false),
            
         ];
     }
@@ -20,7 +21,7 @@ class Chamadas
     {
        
         if (isset($this->arrChamadas[$endpoint])) {
-
+          
             return   $this->arrChamadas[$endpoint];
         }
 
--- generic/JWTAuth.php
+++ generic/JWTAuth.php
@@ -1,39 +1,50 @@
 <?
+
 namespace generic;
 
+use Exception;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-class JWTAuth{
-    private string $key="adlsfh ljslkdfjalskdfaljdhfj23480rfjehjH#&*#####jnaskfj3947*($$&&@_)jhdfbajhfawekjh@#$*$(#@jsfglkçsfjgçoisfgghjki4";
+class JWTAuth
+{
+    private string $key = "adlsfh ljslkdfjalskdfaljdhfj23480rfjehjH#&*#####jnaskfj3947*($$&&@_)jhdfbajhfawekjh@#$*$(#@jsfglkçsfjgçoisfgghjki4";
 
-    public function criarChave($dados){
-        $hora=time();
+    public function criarChave($dados)
+    {
+        $hora = time();
         $payload = [
             'iat' => $hora,
             'exp' => $hora + 180000,
             'uid' => $dados
         ];
 
-        $jwt = JWT::encode($payload,$this->key,'HS256');
+        $jwt = JWT::encode($payload, $this->key, 'HS256');
         return $jwt;
     }
 
-    public function verificar(){
-        if(!isset($_SERVER['HTTP_AUTHORIZATION'])){
-            http_response_code(406);
-            return false;
-        }
-        $autorizacao = $_SERVER['HTTP_AUTHORIZATION'];
-        $token = str_replace('Bearer ','',$autorizacao);
-        $decodificar = Jwt::decode($token,new Key($this->key,'HS256')); //payload
-        $hora = time();
-        if($hora>$decodificar->exp){
-            http_response_code(408);
+    public function verificar()
+    {
+        try {
+            if (!isset($_SERVER['HTTP_AUTHORIZATION'])) {
+                http_response_code(406);
+                return false;
+            }
+            $autorizacao = $_SERVER['HTTP_AUTHORIZATION'];
+            $token = str_replace('Bearer ', '', $autorizacao);
+            $decodificar = Jwt::decode($token, new Key($this->key, 'HS256')); //payload
+            $hora = time();
+            if ($hora > $decodificar->exp) {
+                http_response_code(408);
+                return false;
+            }
+
+            return $decodificar;
+        } catch (Exception $e) {
+            http_response_code(401);
             return false;
         }
 
-        return $decodificar;
+        
     }
 }
-?>
