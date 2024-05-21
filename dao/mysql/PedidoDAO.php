c15ffe8db1b2372f0a807088a635fe22eb09a972
Silovisk <silasaraujo1@hotmail.com>
2024-05-21 18:36:56 -0300

    refactor: Adicionar método show para exibir detalhes de um pedido no PedidoDAO

--- dao/mysql/PedidoDAO.php
+++ dao/mysql/PedidoDAO.php
@@ -52,4 +52,137 @@ class PedidoDAO extends MysqlFactory implements IPedidoDAO
         );
         return $retorno;
     }
+
+    public function show($id)
+    {
+        $pedido = $this->findPedido($id);
+        if (empty($pedido)) {
+            return "Pedido não encontrado";
+        }
+        $sqlPedido = "SELECT * FROM Pedidos where id = $id";
+        $retornoPedido = $this->banco->executar($sqlPedido);
+
+        $data = [
+            "pedido" => $retornoPedido
+        ];
+        return $data;
+    }
+
+    
+    public function update($id, $usuario_id, $restaurante_id, $data_hora)
+    {
+        try {
+            $pedido = $this->findPedido($id);
+            $restauranteDAO = new RestauranteDAO();
+            $restaurante = $restauranteDAO->findRestaurante($restaurante_id);
+
+            $usuarioDAO = new UsuarioDAO();
+            $usuario = $usuarioDAO->findUsuario($usuario_id);
+
+            if (empty($pedido)) {
+                return "Pedido não encontrado";
+            }
+
+            if(empty($usuario)){
+                return "Usuario não encontrado";
+            }
+
+            if (empty($restaurante)) {
+                return "Restaurante não encontrado";
+            }
+
+            $sql ="UPDATE Pedidos SET 
+                    usuario_id = :usuario_id
+                    ,restaurante_id = :restaurante_id
+                    ,data_hora = :data_hora
+                    WHERE id = :id
+                ";
+            $retorno = $this->banco->executar(
+                $sql,
+                [
+                    "usuario_id" => $usuario_id,
+                    "restaurante_id" => $restaurante_id,
+                    "data_hora" => $data_hora,
+                    "id" => $id
+                ]
+            );
+            return $retorno;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $pedido = $this->findPedido($id);
+            if (empty($pedido)) {
+                return "pedido não encontrado";
+            }
+            $sql = "DELETE FROM Pedidos WHERE id = :id";
+            $retorno = $this->banco->executar($sql, ["id" => $id]);
+            return $retorno;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function retornaRestauranteEUsuario($id)
+    {
+
+        $pedido = $this->findAllPedido($id);
+        
+        if (empty($pedido)) {
+            return "Pedido não encontrado";
+        }
+
+        $pedido = $pedido[0];
+        
+        $usuario_id = $pedido['usuario_id'];
+        $restaurante_id = $pedido['restaurante_id'];
+        
+        $usuarioDAO = new UsuarioDAO();
+        $usuario = $usuarioDAO->findAllUsuario($usuario_id);
+        
+        if (empty($usuario)) {
+            return "Usuario não encontrado";
+        }
+
+        $restauranteDAO = new RestauranteDAO();
+        $restaurante = $restauranteDAO->findAllRestaurante($restaurante_id);
+
+        if (empty($restaurante)) {
+            return "Restaurante não encontrado";
+        }
+
+        $data = [
+            'usuario_id' => $usuario_id,
+            [
+                'usuario' => $usuario
+            ],
+            'restaurante_id' => $restaurante_id,
+            [
+                'restaurante' => $restaurante
+            ]            
+        ];
+
+        return $data;
+    }
+
+
+    public function findPedido($id)
+    {
+        $sql = "SELECT p.id from Pedidos p where id = $id";
+        $retorno = $this->banco->executar($sql);
+
+        return $retorno;
+    }
+
+    public function findAllPedido($id)
+    {
+        $sql = "SELECT * from Pedidos p where id = $id";
+        $retorno = $this->banco->executar($sql);
+
+        return $retorno;
+    }
 }
